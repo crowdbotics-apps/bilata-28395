beb7d8d4d52042d420ed65aa5d4e0f49b69b8018
Crowdbotics <team@crowdbotics.com>
2021-07-02 21:43:44 +0000

    Update models

--- backend/bilata_28395/wsgi.py
+++ backend/bilata_28395/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bilata_28395.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bilata_28395.settings")
 
 application = get_wsgi_application()
